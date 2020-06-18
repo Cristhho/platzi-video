cf32052ed45fcc99f191dbe5e2ed73d7320d36f6
cristhian ochoa <cristhho@gmail.com>
2020-06-17 23:44:43 -0500

    header

--- src/components/HelloWorld.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/styles/app.scss';
-
-export default function HelloWorld() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import '../assets/styles/app.scss';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
+}
