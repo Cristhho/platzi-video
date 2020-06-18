6f10fac157fcce00272f42d0f9243509b778dfca
cristhian ochoa <cristhho@gmail.com>
2020-06-17 23:46:18 -0500

    search component

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../assets/styles/components/search.scss';
+
+export default function Search() {
+  return (
+    <section className='main'>
+      <h2 className='main__title'>¿Qué quieres ver hoy?</h2>
+      <input type='text' className='input' placeholder='Buscar...' />
+    </section>
+  );
+}
