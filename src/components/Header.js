0361fd1ad85788e8f358448f8e736416f0c13779
christian <cristhho@gmail.com>
2020-08-02 00:40:05 -0500

    login and logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 
 import '../assets/styles/components/header.scss';
 
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-export default function Header() {
+const header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
   return (
     <header className='header'>
       <Link to='/'>
@@ -14,14 +23,26 @@ export default function Header() {
       </Link>
       <div className='header__menu'>
         <div className='header__menu--profile'>
-          <img src={userIcon} alt='' />
+          <img src={hasUser ? gravatar(user.email) : userIcon} alt={hasUser ? user.email : ''} />
           <p>Perfil</p>
         </div>
         <ul>
-          <li><Link to='/login'>Iniciar Sesión</Link></li>
-          <li><a href='/'>Cuenta</a></li>
+          {hasUser ? <li><a href='/'>{user.name}</a></li> : null}
+          {hasUser ? <li><a href='#logout' onClick={handleLogout}>Cerrar sesión</a></li> : <li><Link to='/login'>Iniciar Sesión</Link></li>}
         </ul>
       </div>
     </header>
   );
-}
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(header);
